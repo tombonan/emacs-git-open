2bc16018bdc5236792805abd9f545b61d67d8460
Tom Bonan <tombonan018@gmail.com>
2023-04-03 20:40:27 -0600

    Initial commit
    Update README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# emacs-git-open
+
+Emacs package to open a file on a git remote. Inspired by [git-open](https://github.com/paulirish/git-open)
---
+++ emacs-git-open.el
@@ -0,0 +1,29 @@
+;;; emacs-git-open.el --- Open a git repo remote website in browser -*- lexical-binding: t -*-
+
+;; Copyright (C) 2023 Tom Bonan
+;; Author: Tom Bonan <emacs-git-open@tombonan.me>
+;; Version: 0.1.0
+;; Keywords: git, github
+;; URL: https://github.com/tombonan/emacs-git-open
+;; Package-Requires: ((emacs "24.3"))
+
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Change Log:
+
+;;; Code:
